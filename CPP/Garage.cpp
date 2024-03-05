6d0b74293a0d21481cc2394a2682bd6b8aabbc5f
ThemySabri <themysabriii@gmail.com>
2024-03-05 11:19:11 +0700

    mod: nambah janji cpp dan lengkapin readme

--- CPP/Garage.cpp
+++ CPP/Garage.cpp
@@ -1,3 +1,12 @@
+/*
+Bismillah
+Saya Themy Sabri Syuhada dengan NIM 2203903. 
+Demi keberkahan-Nya, saya berjanji mengerjakan 
+latihan praktikum 4 DPBO dengan jujur 
+dan tidak melakukan kecurangan seperti yang telah dispesifikasikan.
+Aamiin.
+*/
+
 #pragma once
 
 #include <iostream>
--- CPP/ParkingLot.cpp
+++ CPP/ParkingLot.cpp
@@ -1,8 +1,17 @@
+/*
+Bismillah
+Saya Themy Sabri Syuhada dengan NIM 2203903.
+Demi keberkahan-Nya, saya berjanji mengerjakan
+latihan praktikum 4 DPBO dengan jujur
+dan tidak melakukan kecurangan seperti yang telah dispesifikasikan.
+Aamiin.
+*/
+
 #pragma once
 
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 class ParkingLot
 {
